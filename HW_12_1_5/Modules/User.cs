7984ddbcee35677e37dbd67867e27b6739779bc2
OGislambek <ogislambek@gmail.com>
2024-11-09 22:09:20 +0300

    Добавьте файлы проекта.

---
+++ HW_12_1_5/Modules/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_12_1_5.Modules
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public bool IsPremium { get; set; }
+
+        public User(string name, string login, bool isPremium)
+        {
+            Name = name;
+            Login = login;
+            IsPremium = isPremium;
+        }
+
+        public void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com;");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда;");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.\n\n");
+            Thread.Sleep(3000);
+        }
+
+        public void GreetingPremiumUser(string name, string login)
+        {
+            Console.WriteLine($"Приветствуем Вас, {name}({login}),");
+            Thread.Sleep(1000);
+            Console.WriteLine("Ваша премиальная подписка активна.\n\n");
+            Thread.Sleep(3000);
+
+        }
+    }
+}
---
+++ HW_12_1_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using HW_12_1_5.Modules;
+
+namespace HW_12_1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>() { new User("Олег", "OOO Leg", false), new User("Татьяна", "Thotiana123", false), new User("Григорий", "OGrigory", true), new User("Николай", "NikolaII", false) };
+
+            Thread.Sleep(2000);
+            foreach (User user in users)
+            {
+                if (user.IsPremium == true)
+                {
+                    user.GreetingPremiumUser(user.Name, user.Login);
+                }
+                else
+                {
+                    Console.WriteLine($"{user.Name}({user.Login}),");
+                    Thread.Sleep(1000);
+                    user.ShowAds();
+                }
+            }
+        }
+    }
+}
